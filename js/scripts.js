5ca65176d3728587ef71923179e0a0ad9f128ebb
DaniLott1988 <danilott1988@gmail.com>
2021-09-16 12:58:22 +0200

    Update scripts.js
    Input examples and comments about the use of "\".

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,9 @@ alert('Hello World!');
 
 let favoriteFood = 'Pao de Queijo';
 document.write(favoriteFood);
+
+/* Use backslash to allow a special symbol to be interpreted as regular code-text */
+
+let escapedText = "\"Yes!\", that's for sure.";
+let escapedText2 = '"Yes!", that\'s for sure.';
+let escapedText3 = 'This is \\ it!'

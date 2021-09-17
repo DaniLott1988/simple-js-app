23b52cd9881d5f15f3d02be713d4c34524f7f04d
DaniLott1988 <danilott1988@gmail.com>
2021-09-17 17:02:36 +0200

    Update scripts.js
    Input Task 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,73 @@
 let pokemonList = [
   {
-    name="Bulbasaur",
-    type=[
+    name:"Bulbasaur",
+    type:[
       'grass',
       'poison'
     ],
-    height=0.7
+    height:0.7
   },
   {
-    name="Ivysaur",
-    type=[
+    name:"Ivysaur",
+    type:[
       'grass',
       'poison'
     ],
-    height=1
+    height:1.0
   },
   {
-    name="Venusaur",
-    type=[
+    name:"Venusaur",
+    type:[
       'grass',
       'poison'
     ],
-    height=2
+    height:2.0
   },
   {
-    name="Charmander",
-    type="fire",
-    height=0.6
+    name:"Charmander",
+    type:"fire",
+    height:0.6
   },
   {
-    name="Charmeleon",
-    type="fire",
-    height=1.1
+    name:"Charmeleon",
+    type:"fire",
+    height:1.1
   },
   {
-    name="Charizard",
-    type=[
+    name:"Charizard",
+    type:[
       'fire',
       'flying'
     ],
-    height=1.7
+    height:1.7
   },
   {
-    name="Squirtle",
-    type="water",
-    height=0.5
+    name:"Squirtle",
+    type:"water",
+    height:0.5
   },
   {
-    name="Wartortle",
-    type="water",
-    height=1
+    name:"Wartortle",
+    type:"water",
+    height:1
   },
   {
-    name="Blastoise",
-    type="water",
-    height=1.6
+    name:"Blastoise",
+    type:"water",
+    height:1.6
   }
-]
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  let pokemonNa = pokemonList[i].name;
+  let pokemonHe = pokemonList[i].height;
+  if (pokemonHe >= 1.9) {
+    document.write(
+      '<p>' + pokemonNa + ' ' + '(Height: ' + pokemonHe + ')' + "Woa! You found a big one!" + '</p>'
+    );
+  } else {
+    document.write(
+      '<p>' + pokemonNa + ' ' + '(Height: ' + pokemonHe + ')' + '</p>'
+    );
+  }
+}

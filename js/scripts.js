94830a36f60f3be5f5ee01d3114081a5c7c049ab
DaniLott1988 <danilott1988@gmail.com>
2021-09-17 17:14:03 +0200

    Update scripts.js
    Added comments

--- js/scripts.js
+++ js/scripts.js
@@ -61,10 +61,12 @@ let pokemonList = [
 for (let i = 0; i < pokemonList.length; i++) {
   let pokemonNa = pokemonList[i].name;
   let pokemonHe = pokemonList[i].height;
+//start of the conditional with if, a condition must be met in order to obtain the result A
   if (pokemonHe >= 1.9) {
     document.write(
       '<p>' + pokemonNa + ' ' + '(Height: ' + pokemonHe + ')' + " - Woa! You found a big one!" + '</p>'
     );
+    //if the conditional "if" is not met, it will result in the else condition being fulfilled in this case
   } else {
     document.write(
       '<p>' + pokemonNa + ' ' + '(Height: ' + pokemonHe + ')' + '</p>'

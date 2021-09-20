40f5813f1c75929be88f16e1c8a542e613dbbf8f
DaniLott1988 <danilott1988@gmail.com>
2021-09-20 19:47:59 +0200

    Update scripts.js
    Added IIFE with GETALL and ADD functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,76 @@
-let pokemonList = [
-  {
-    name:"Bulbasaur",
-    type:[
-      'grass',
-      'poison'
-    ],
-    height:0.7
-  },
-  {
-    name:"Ivysaur",
-    type:[
-      'grass',
-      'poison'
-    ],
-    height:1.0
-  },
-  {
-    name:"Venusaur",
-    type:[
-      'grass',
-      'poison'
-    ],
-    height:2.0
-  },
-  {
-    name:"Charmander",
-    type:"fire",
-    height:0.6
-  },
-  {
-    name:"Charmeleon",
-    type:"fire",
-    height:1.1
-  },
-  {
-    name:"Charizard",
-    type:[
-      'fire',
-      'flying'
-    ],
-    height:1.7
-  },
-  {
-    name:"Squirtle",
-    type:"water",
-    height:0.5
-  },
-  {
-    name:"Wartortle",
-    type:"water",
-    height:1
-  },
-  {
-    name:"Blastoise",
-    type:"water",
-    height:1.6
+let pokemonRepository = (
+  function () {
+    let pokemonList = [
+      {
+        name:"Bulbasaur",
+        type:[
+          'grass',
+          'poison'
+        ],
+        height:0.7
+      },
+      {
+        name:"Ivysaur",
+        type:[
+          'grass',
+          'poison'
+        ],
+        height:1.0
+      },
+      {
+        name:"Venusaur",
+        type:[
+          'grass',
+          'poison'
+        ],
+        height:2.0
+      },
+      {
+        name:"Charmander",
+        type:"fire",
+        height:0.6
+      },
+      {
+        name:"Charmeleon",
+        type:"fire",
+        height:1.1
+      },
+      {
+        name:"Charizard",
+        type:[
+          'fire',
+          'flying'
+        ],
+        height:1.7
+      },
+      {
+        name:"Squirtle",
+        type:"water",
+        height:0.5
+      },
+      {
+        name:"Wartortle",
+        type:"water",
+        height:1
+      },
+      {
+        name:"Blastoise",
+        type:"water",
+        height:1.6
+      }
+    ];
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    };
+    function getAll() {
+      return pokemonList;
+    };
+    return {
+      add: add,
+      getAll: getAll
+    };
   }
-];
+)
 
 pokemonList.forEach(
   function (currentPokemon) {
